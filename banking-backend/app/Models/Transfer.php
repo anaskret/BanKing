0e8145396766dde0ddc942a138e0c1a358fd6bc4
dMichalak1 <48916443+dMichalak1@users.noreply.github.com>
2021-07-01 17:37:14 +0200

    checkDateTransfer

--- banking-backend/app/Models/Transfer.php
+++ banking-backend/app/Models/Transfer.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,7 +9,7 @@ class Transfer extends Model
 {
     use HasFactory;
     protected $table = 'transfers';
-  
+   
      
     protected $fillable = [
         'yourAccountNumber',
@@ -25,5 +25,25 @@ class Transfer extends Model
         'isComplete',
 
     ];
+    // SPRAWDZANIE CZY PRZELEW JUZ DOTARŁ
+    public function checkTransfer($transfers)
+    {
+        foreach($transfers as $transfer)
+        {
+
+            $today = new DateTime(date("Y-m-d"));
+            $date2 = new DateTime($transfer->date);
+
+            if($today >= $date2)
+            {
+                $transfer->isComplete=true;
+            }
+            else
+            {
+                $transfer->isComplete=false;
+            }
+            $transfer->update();
+        }
+    }
 
 }
